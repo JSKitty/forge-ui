9d595b4456d38ad7244294755196a5511f85cef8
JSKitty <loopylew113@protonmail.com>
2020-01-04 04:18:36 +0000

    Merge latest Forge codebase

--- index.js
+++ index.js
@@ -28,16 +28,16 @@ let itemsToValidate = [];
 let explorer = "";
 
 // Get a peer object from our list by it's host or index
-function getPeer(peerArg) {
-    for (let i = 0; i < peers.length; i++) {
+function getPeer (peerArg) {
+    for (let i=0; i<peers.length; i++) {
         if (peers[i].host === peerArg || peers[i].index === peerArg) return peers[i];
     }
     return null;
 }
 
 // Updates the contents of a peer object
-function updatePeer(peerArg) {
-    for (let i = 0; i < peers.length; i++) {
+function updatePeer (peerArg) {
+    for (let i=0; i<peers.length; i++) {
         if (peers[i].host === peerArg.host || peers[i].index === peerArg.index) {
             peers[i] = peerArg;
             return true;
@@ -51,7 +51,7 @@ const authToken = nanoid();
 console.info("PRIVATE: Your local auth token is '" + authToken + "'");
 
 // Checks if a private HTTP request was authenticated
-function isAuthed(req) {
+function isAuthed (req) {
     if (req.body.auth === authToken) return true;
     return false;
 }
@@ -63,15 +63,16 @@ async function asyncForEach(array, callback) {
 }
 
 // Validates if an item is genuine
-async function isItemValid(nItem) {
+async function isItemValid (nItem, approve = false) {
     try {
-        console.info("getrawtransaction " + nItem.tx + " 1");
         let rawTx = await zenzo.call("getrawtransaction", nItem.tx, 1);
         if (!rawTx || !rawTx.vout || !rawTx.vout[0]) {
             console.warn('Forge: Item "' + nItem.name + '" is not in the blockchain.');
+            disproveItem(nItem);
+            addInvalidationScore(nItem, 2);
             return false;
         }
-        for (let i = 0; i < rawTx.vout.length; i++) {
+        for (let i=0; i<rawTx.vout.length; i++) {
             if (rawTx.vout[i].value === nItem.value) {
                 if (rawTx.vout[i].scriptPubKey.addresses.includes(nItem.address)) {
                     console.log("Found pubkey of item...");
@@ -84,23 +85,31 @@ async function isItemValid(nItem) {
                             res = JSON.parse(res.text);
                             if (res.length === 0) {
                                 console.warn("UTXO couldn't be found, item '" + nItem.name + "' has no UTXOs");
+                                disproveItem(nItem);
+                                addInvalidationScore(nItem, 5);
                                 return false; // UTXO has been spent
                             }
-                            for (let i = 0; i < res.length; i++) {
+                            for (let i=0; i<res.length; i++) {
                                 if (res[i].txid === nItem.tx) {
                                     console.warn("Found unspent UTXO collateral...");
+                                    if (approve) approveItem(nItem);
                                     return true; // Found unspent collateral UTXO
                                 }
                             }
-                            console.info("STUFFS: " + JSON.stringify(res));
                             console.warn("UTXO couldn't be found, item '" + nItem.name + "' does not have a collateral UTXO");
+                            disproveItem(nItem);
+                            addInvalidationScore(nItem, 5);
                             return false; // Couldn't find unspent collateral UTXO
                         } else {
                             console.info("Hash is not genuine...");
+                            disproveItem(nItem);
+                            addInvalidationScore(nItem, 12.5);
                             return false;
                         }
                     } else {
                         console.info("Sig is not genuine...");
+                        disproveItem(nItem);
+                        addInvalidationScore(nItem, 12.5);
                         return false;
                     }
                 }
@@ -113,14 +122,14 @@ async function isItemValid(nItem) {
     return false;
 }
 
-async function validateItems(revalidate = false) {
+async function validateItems (revalidate = false) {
     let validated = 0;
     await asyncForEach(((revalidate) ? items : itemsToValidate), async (item) => {
-        let res = await isItemValid(item);
+        let res = await isItemValid(item, true);
         if (res) {
             validated++;
         } else {
-            for (let i = 0; i < items.length; i++) {
+            for (let i=0; i<items.length; i++) {
                 if (items[i].tx === item.tx) {
                     items.splice(i, 1); // Erase invalid item from our list
                     console.warn("Erased bad item! (" + item.name + " - " + item.tx + ")");
@@ -131,7 +140,7 @@ async function validateItems(revalidate = false) {
     return validated;
 }
 
-async function validateItemBatch(res, nItems, reply) {
+async function validateItemBatch (res, nItems, reply) {
     await asyncForEach(nItems, async (nItem) => {
         // Check all values are valid
         if (nItem.tx.length !== 64) return console.warn("Forge: Received invalid item, TX length is not 64.");
@@ -141,22 +150,79 @@ async function validateItemBatch(res, nItems, reply) {
         if (nItem.value < 0.01) return console.warn("Forge: Received invalid item, value is below minimum.");
         if (nItem.hash.length !== 64) return console.warn("Forge: Received invalid item, hash length is not 64.");
 
-        let valid = await isItemValid(nItem);
+        let valid = await isItemValid(nItem, true);
         if (!valid) {
             return console.error("Forge: Received item is not genuine, ignored.");
         }
         if (getItem(nItem.hash) === null) {
-            items.push(nItem);
-            console.info("New item received! (" + nItem.name + ") We have " + items.length + " items.");
+            console.info("New item received from peer! (" + nItem.name + ") We have " + items.length + " items.");
             if (reply) res.send("Thanks!");
         }
     });
     return true;
 }
 
+// Approve an item as valid, moving it to the main items DB and removing it from the pending list
+function approveItem(item) {
+    for (let i=0; i<itemsToValidate.length; i++) {
+        if (item.tx === itemsToValidate[i].tx) {
+            items.push(item);
+            itemsToValidate.splice(i, 1);
+            console.info("An item has been approved!\n - Item '" + item.name + "' (" + item.tx + ") has been approved and appended as a verified item.");
+        }
+    }
+}
+
+// Disprove an item, moving it out of the main items DB and to the pending list
+function disproveItem(item) {
+    for (let i=0; i<items.length; i++) {
+        if (item.tx === items[i].tx) {
+            itemsToValidate.push(items[i]);
+            items.splice(i, 1);
+            console.info("An item has been disproved!\n - Item '" + item.name + "' (" + item.tx + ") has been removed as a verified item and is now pending.");
+        }
+    }
+}
+
+// Increments the invalidation score of an item, if this score reaches 25, the item is considered irreversibly invalid, and removed from the DB permanently
+function addInvalidationScore(item, score) {
+    for (let i=0; i<items.length; i++) {
+        if (item.tx === items[i].tx) {
+            if (!items[i].invalidScore) items[i].invalidScore = 0;
+            items[i].invalidScore += score;
+            item.invalidScore = items[i].invalidScore;
+            console.info("An invalidation score of '" + score + "' has been applied to '" + item.name + "', now totalling '" + items[i].invalidScore + "' invalidation score.");
+            if (item.invalidScore >= 25) {
+                items.splice(i, 1);
+                console.info(" - Item has been abandoned due to exceeding the invalidation score threshold.");
+            }
+        }
+    }
+    for (let i=0; i<itemsToValidate.length; i++) {
+        if (item.tx === itemsToValidate[i].tx) {
+            if (!itemsToValidate[i].invalidScore) itemsToValidate[i].invalidScore = 0;
+            itemsToValidate[i].invalidScore += score;
+            item.invalidScore = itemsToValidate[i].invalidScore;
+            console.info("An invalidation score of '" + score + "' has been applied to '" + item.name + "', now totalling '" + itemsToValidate[i].invalidScore + "' invalidation score.");
+            if (item.invalidScore >= 25) {
+                itemsToValidate.splice(i, 1);
+                console.info(" - Item has been abandoned due to exceeding the invalidation score threshold.");
+            }
+        }
+    }
+}
+
+// Cleans a list of items of their local-node data
+function cleanItems (itemList) {
+    for (let i=0; i<itemList.length; i++) {
+        delete itemList[i].invalidScore;
+    }
+    return itemList;
+}
+
 // Get an item object from our list by it's hash
 function getItem(itemArg) {
-    for (let i = 0; i < items.length; i++) {
+    for (let i=0; i<items.length; i++) {
         if (items[i].hash === itemArg) return items[i];
     }
     return null;
@@ -238,7 +304,7 @@ class Peer {
     sendItems(itemz) {
         return superagent
             .post(this.host + "/forge/receive")
-            .send(itemz)
+            .send(cleanItems(itemz))
             .then((res) => {
                 this.lastPing = Date.now();
                 this.setStale(false);
@@ -261,7 +327,7 @@ class Peer {
                 this.setStale(false);
                 let data = JSON.parse(res.text);
                 console.info(`Peer "${this.host}" (${this.index}) sent items (${data.items.length} Items, ${data.pendingItems.length} Pending Items)`);
-                validateItemBatch(null, data.items.concat(data.pendingItems), false).then(done => {
+                validateItemBatch(null, cleanItems(data.items.concat(data.pendingItems)), false).then(done => {
                     if (done) {
                         console.info(`Synced with peer "${this.host}", we now have ${items.length} valid & ${itemsToValidate.length} pending items!`);
                     } else console.warn(`Failed to sync with peer "${this.host}"`);
@@ -322,7 +388,7 @@ app.post('/forge/sync', (req, res) => {
         req.peer.getItems();
     }
 
-    let obj = { items: items, pendingItems: itemsToValidate };
+    let obj = {items: items, pendingItems: itemsToValidate};
     res.send(JSON.stringify(obj));
 });
 
@@ -333,16 +399,16 @@ app.post('/forge/inventory', (req, res) => {
     let ourPendingItems = [];
 
     // Find our validated items
-    for (let i = 0; i < items.length; i++) {
+    for (let i=0; i<items.length; i++) {
         if (items[i].address === addy) ourItems.push(items[i]);
     }
 
     // Find our pending items (Mempool items, these can be optionally included in fast-paced games)
-    for (let i = 0; i < itemsToValidate.length; i++) {
+    for (let i=0; i<itemsToValidate.length; i++) {
         if (itemsToValidate[i].address === addy) ourPendingItems.push(itemsToValidate[i]);
     }
 
-    let obj = { items: ourItems, pendingItems: ourPendingItems };
+    let obj = {items: ourItems, pendingItems: ourPendingItems};
     res.send(JSON.stringify(obj));
 });
 
@@ -356,7 +422,7 @@ app.post('/forge/account', (req, res) => {
     if (!isAuthed(req)) return console.warn("Forge: A non-authorized Forge was made by '" + ip + "', ignoring.");
 
     zenzo.call("getinfo").then(info => {
-        let obj = { forge_address: addy, balance: info.balance, wallet_version: info.version };
+        let obj = {forge_address: addy, balance: info.balance, wallet_version: info.version};
         res.json(obj);
     })
 });
@@ -388,7 +454,12 @@ app.post('/forge/create', (req, res) => {
             nItem.hash = hash(nItem.tx + nItem.sig + nItem.address + nItem.name + nItem.value);
             console.log("Forge: Item Created!\n- TX: " + nItem.tx + "\n- Signature: " + nItem.sig + "\n- Name: " + nItem.name + "\n- Value: " + nItem.value + " ZNZ\n- Hash: " + nItem.hash);
             itemsToValidate.push(nItem);
-            res.json(nItem);
+            zenzo.call("gettransaction", txid).then(rawtx => {
+                zenzo.call("lockunspent", false, [{"txid": txid, "vout": rawtx.details[0].vout}]).then(didLock => {
+                    if (didLock) console.info("- Item collateral was successfully locked in ZENZO Coin Control.");
+                    res.json(nItem);
+                }).catch(console.error);
+            }).catch(console.error);
         }).catch(console.error);
     }).catch(console.error);
 });
@@ -396,7 +467,7 @@ app.post('/forge/create', (req, res) => {
 // Forge Items
 // The endpoint for getting a list of validated and pending items
 app.post('/forge/items', (req, res) => {
-    let obj = { items: items, pendingItems: itemsToValidate };
+    let obj = {items: items, pendingItems: itemsToValidate};
     res.json(obj);
 });
 
@@ -406,14 +477,14 @@ app.listen(80);
 /* ------------------ I/O Operations ------------------ */
 
 // Write data to a specified file
-async function toDisk(file, data, isJson) {
+async function toDisk (file, data, isJson) {
     if (isJson) data = JSON.stringify(data);
     await fs.writeFileSync(appdata + 'data/' + file, data);
     return true;
 }
 
 // Read data from a specified file
-async function fromDisk(file, isJson) {
+async function fromDisk (file, isJson) {
     if (!fs.existsSync(appdata + 'data/' + file)) return null;
     let data = await fs.readFileSync(appdata + 'data/' + file, "utf8");
     if (isJson) data = JSON.parse(data);
@@ -424,7 +495,7 @@ async function fromDisk(file, isJson) {
 /* Core Node Mechanics */
 // First! Let's bootstrap the validator with seednodes
 const seednodes = ["45.12.32.114", "144.91.87.251:8000"];
-for (let i = 0; i < seednodes.length; i++) {
+for (let i=0; i<seednodes.length; i++) {
     let seednode = new Peer(seednodes[i]);
     seednode.connect(true);
 }
@@ -443,7 +514,6 @@ if (!fs.existsSync(appdata + 'data/')) {
             console.warn("Init: file 'items.json' missing from disk, ignoring...");
         else
             items = nDiskItems;
-
         fromDisk("pending_items.json", true).then(nDiskPendingItems => {
             if (nDiskPendingItems === null)
                 console.warn("Init: file 'pending_items.json' missing from disk, ignoring...");
@@ -456,7 +526,7 @@ if (!fs.existsSync(appdata + 'data/')) {
 }
 
 // Start the "janitor" loop to ping peers, validate items and save to disk at intervals
-let janitor = setInterval(function () {
+let janitor = setInterval(function() {
     // Ping peers
     peers.forEach(peer => {
         peer.ping();
@@ -509,7 +579,7 @@ function rpcError() {
 
 // Load variables from disk config
 fromDisk("config.json", true).then(config => {
-    let rpcAuth = { user: config.wallet.user, pass: config.wallet.pass, port: config.wallet.port };
+    let rpcAuth = {user: config.wallet.user, pass: config.wallet.pass, port: config.wallet.port};
     addy = config.wallet.address;
     zenzo = new RPC('http://' + rpcAuth.user + ':' + rpcAuth.pass + '@localhost:' + rpcAuth.port);
     explorer = config.blockbook;
